aa63d8e4e40b5fe58d01a22dda4decfcf8546fe6
James Fisher <fisher.jc@gmail.com>
2018-03-28 14:54:51 -0400

    updated united code for new practice

--- R/united.R
+++ R/united.R
@@ -8,6 +8,7 @@
 #' y <- united(x) %>% write_csv(y, "y.csv", na = "")
 
 united <- function(x) {
+
   # Concatenates Provider information
   x$CURR_PCP_FULL_NAME <- paste(x$PROV_LNAME, x$PROV_FNAME, sep=", ")
 
@@ -41,23 +42,6 @@ united <- function(x) {
   x <- rename(x, c(PROV_ZIP = "CURR_PCP_ZIP"))
   x <- rename(x, c(PAYEE_NAME = "VEND_FULL_NAME"))
 
-  # Maps languages in the united file to the full name of the language
-  x$MEMB_LANGUAGE <- as.character(x$MEMB_LANGUAGE)
-  x$MEMB_LANGUAGE[x$MEMB_LANGUAGE=="ARA"] <- "Arabic"
-  x$MEMB_LANGUAGE[x$MEMB_LANGUAGE=="CHI"] <- "Chinese"
-  x$MEMB_LANGUAGE[x$MEMB_LANGUAGE=="ENG"] <- "English"
-  x$MEMB_LANGUAGE[x$MEMB_LANGUAGE=="FRE"] <- "French"
-  x$MEMB_LANGUAGE[x$MEMB_LANGUAGE=="HEB"] <- "Hebrew"
-  x$MEMB_LANGUAGE[x$MEMB_LANGUAGE=="ITA"] <- "Italian"
-  x$MEMB_LANGUAGE[x$MEMB_LANGUAGE=="KOR"] <- "Korean"
-  x$MEMB_LANGUAGE[x$MEMB_LANGUAGE=="N/A"] <- ""
-  x$MEMB_LANGUAGE[x$MEMB_LANGUAGE=="PER"] <- "Persian"
-  x$MEMB_LANGUAGE[x$MEMB_LANGUAGE=="POR"] <- "Portuegese"
-  x$MEMB_LANGUAGE[x$MEMB_LANGUAGE=="SPA"] <- "Spanish"
-  x$MEMB_LANGUAGE[x$MEMB_LANGUAGE=="TUR"] <- "Turkish"
-  x$MEMB_LANGUAGE[x$MEMB_LANGUAGE=="UNK"] <- "Unknown"
-  x$MEMB_LANGUAGE[x$MEMB_LANGUAGE=="VIE"] <- "Vietnamese"
-
   # Adds text identifiers to Subscriber IDs
   x$SUBSCRIBER_ID <- paste("U", x$SUBSCRIBER_ID, sep="")
 
@@ -78,6 +62,28 @@ united <- function(x) {
   # Deletes entries with the wrong vendor names
   x <- subset(x, !(VEND_FULL_NAME=="CHILD REGIONAL/CAMDEN"))
 
+  # Subset Cooper Cherry Hill practices
+  pattern_1050 <- c('1050', 'KING') # Cooper 1050 N Kings
+  pattern_1103 <- c('1103', 'KING') # Cooper 1103 Kings
+  pattern_1210 <- c('1210', 'BRACE') # Cooper Brace
+
+  y <- subset(x, CURR_PCP_CITY=="CHERRY HILL")
+
+  y2 <- subset(y, y$VEND_FULL_NAME == "COOPER FAMILY MEDICINE" |y$VEND_FULL_NAME == "COOPER PHYSICIANS OFFICES")
+
+  y2$VEND_FULL_NAME[str_detect(y2$CURR_PCP_ADDRESS_LINE_1, pattern_1050)] <- "COOPER 1050 KINGS"
+  y2$VEND_FULL_NAME[str_detect(y2$CURR_PCP_ADDRESS_LINE_1, pattern_1103)] <- "COOPER 1103 KINGS"
+  y2$VEND_FULL_NAME[str_detect(y2$CURR_PCP_ADDRESS_LINE_1, pattern_1210)] <- "COOPER 1210 BRACE"
+
+  y2$PRACTICE[y2$VEND_FULL_NAME == "COOPER 1050 KINGS"] <- "Cooper 1050 Kings"
+  y2$PRACTICE[y2$VEND_FULL_NAME == "COOPER 1103 KINGS"] <- "Cooper 1103 Kings"
+  y2$PRACTICE[y2$VEND_FULL_NAME == "COOPER 1210 BRACE"] <- "Cooper 1210 Brace"
+
+  y2$PAYER <- "UNITED"
+  y2$Source <- "United"
+
+  y2 <- y2 %>% filter(is.na(PRACTICE) == F)
+
   # Keeps only where PCP City is Camden, Pennsauken, or Oaklyn, and keeps all of CamCare
   x <- subset(x, CURR_PCP_CITY=="CAMDEN" | CURR_PCP_CITY=="CADMEN" |CURR_PCP_CITY=="CANDEM" | CURR_PCP_CITY=="PENNSAUKEN"| CURR_PCP_CITY=="OAKLYN" | VEND_FULL_NAME=="CAMCARE HEALTH CORPORATION")
 
@@ -88,85 +94,77 @@ united <- function(x) {
   x$PAYER <- "UNITED"
   x$Source <- "United"
 
-  # Sorts columns in x A-Z
+  # Sorts columns in x and y3 A-Z
   x <- x[,order(names(x))]
-
-  # Merges x data
-  AllPayers <- rbind(x)
+  y3 <- y2[,order(names(y2))]
 
   # Converts Current PCP City to all capital letters
-  AllPayers$CURR_PCP_CITY <- toupper(AllPayers$CURR_PCP_CITY)
+  x$CURR_PCP_CITY <- toupper(x$CURR_PCP_CITY)
 
   # Renames vendors to match Current PCP City
-  AllPayers$VEND_FULL_NAME[AllPayers$VEND_FULL_NAME == "LOURDES MEDICAL ASSOCIATES" & AllPayers$CURR_PCP_CITY == "CAMDEN"] <- "LOURDES MEDICAL ASSOCIATES_CAMDEN"
-  AllPayers$VEND_FULL_NAME[AllPayers$VEND_FULL_NAME == "LOURDES MEDICAL ASSOCIATES PA" & AllPayers$CURR_PCP_CITY == "CAMDEN"] <- "LOURDES MEDICAL ASSOCIATES_CAMDEN"
-  AllPayers$VEND_FULL_NAME[AllPayers$VEND_FULL_NAME == "LOURDES MEDICAL ASSOCIATES" & AllPayers$CURR_PCP_CITY == "PENNSAUKEN"] <- "LOURDES MEDICAL ASSOCIATES_PENNSAUKEN"
-  AllPayers$VEND_FULL_NAME[AllPayers$VEND_FULL_NAME == "LOURDES MEDICAL ASSOCIATES PA" & AllPayers$CURR_PCP_CITY == "PENNSAUKEN"] <- "LOURDES MEDICAL ASSOCIATES_PENNSAUKEN"
-  AllPayers$VEND_FULL_NAME[AllPayers$VEND_FULL_NAME == "OSBORN FAMILY PRACTICE" & AllPayers$CURR_PCP_CITY == "CAMDEN"] <- "OSBORN FAMILY PRACTICE_CAMDEN"
-  AllPayers$VEND_FULL_NAME[AllPayers$VEND_FULL_NAME == "THE OSBORN FAMILY HEALTH CENTER" & AllPayers$CURR_PCP_CITY == "CAMDEN"] <- "OSBORN FAMILY PRACTICE_CAMDEN"
-  AllPayers$VEND_FULL_NAME[AllPayers$VEND_FULL_NAME == "THE OSBORN FAMILY HEALTH CENTER INC" & AllPayers$CURR_PCP_CITY == "CAMDEN"] <- "OSBORN FAMILY PRACTICE_CAMDEN"
-  AllPayers$VEND_FULL_NAME[AllPayers$VEND_FULL_NAME == "BROADWAY FAMILY PRACTICE" & AllPayers$CURR_PCP_CITY == "CAMDEN"] <- "RELIANCE BROADWAY_CAMDEN"
-  AllPayers$VEND_FULL_NAME[AllPayers$VEND_FULL_NAME == "BROADWAY FAMILY PRACTICE" & AllPayers$CURR_PCP_CITY == "PENNSAUKEN"] <- "RELIANCE BROADWAY_PENNSAUKEN"
-  AllPayers$VEND_FULL_NAME[AllPayers$VEND_FULL_NAME == "COOPER PHYSICIANS OFFICES" & AllPayers$CURR_PCP_CITY == "OAKLYN"] <- "COOPER WOODLYNNE"
+  x$VEND_FULL_NAME[x$VEND_FULL_NAME == "LOURDES MEDICAL ASSOCIATES" & x$CURR_PCP_CITY == "CAMDEN"] <- "LOURDES MEDICAL ASSOCIATES_CAMDEN"
+  x$VEND_FULL_NAME[x$VEND_FULL_NAME == "LOURDES MEDICAL ASSOCIATES PA" & x$CURR_PCP_CITY == "CAMDEN"] <- "LOURDES MEDICAL ASSOCIATES_CAMDEN"
+  x$VEND_FULL_NAME[x$VEND_FULL_NAME == "LOURDES MEDICAL ASSOCIATES" & x$CURR_PCP_CITY == "PENNSAUKEN"] <- "LOURDES MEDICAL ASSOCIATES_PENNSAUKEN"
+  x$VEND_FULL_NAME[x$VEND_FULL_NAME == "LOURDES MEDICAL ASSOCIATES PA" & x$CURR_PCP_CITY == "PENNSAUKEN"] <- "LOURDES MEDICAL ASSOCIATES_PENNSAUKEN"
+  x$VEND_FULL_NAME[x$VEND_FULL_NAME == "OSBORN FAMILY PRACTICE" & x$CURR_PCP_CITY == "CAMDEN"] <- "OSBORN FAMILY PRACTICE_CAMDEN"
+  x$VEND_FULL_NAME[x$VEND_FULL_NAME == "THE OSBORN FAMILY HEALTH CENTER" & x$CURR_PCP_CITY == "CAMDEN"] <- "OSBORN FAMILY PRACTICE_CAMDEN"
+  x$VEND_FULL_NAME[x$VEND_FULL_NAME == "THE OSBORN FAMILY HEALTH CENTER INC" & x$CURR_PCP_CITY == "CAMDEN"] <- "OSBORN FAMILY PRACTICE_CAMDEN"
+  x$VEND_FULL_NAME[x$VEND_FULL_NAME == "BROADWAY FAMILY PRACTICE" & x$CURR_PCP_CITY == "CAMDEN"] <- "RELIANCE BROADWAY_CAMDEN"
+  x$VEND_FULL_NAME[x$VEND_FULL_NAME == "BROADWAY FAMILY PRACTICE" & x$CURR_PCP_CITY == "PENNSAUKEN"] <- "RELIANCE BROADWAY_PENNSAUKEN"
+  x$VEND_FULL_NAME[x$VEND_FULL_NAME == "COOPER PHYSICIANS OFFICES" & x$CURR_PCP_CITY == "OAKLYN"] <- "COOPER WOODLYNNE"
 
   # Maps to practices
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "ACOSTA RAMON"] <- "Acosta"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "RELIANCE BROADWAY_CAMDEN"] <- "Reliance Broadway"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "NEW JERSEY MEDICAL AND HEALTH ASSOCIATES LLC"] <- "Reliance Broadway"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "RELIANCE MEDICAL GROUP"] <- "Reliance Broadway"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "RELIANCE MEDICAL GROUP LLC"] <- "Reliance Broadway"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "RELIANCE BROADWAY_PENNSAUKEN"] <- "Reliance Pennsauken"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "CAMCARE HEALTH CORPORATION"] <- "CAMcare"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "COOPER AMBULATORY PEDIATRICS"] <- "Cooper Pediatrics"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "COOPER FAMILY MEDICINE"] <- "Cooper Family"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "COOPER FAMILY MEDICINE PC"] <- "Cooper Family"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "COOPER PEDIATRICS"] <- "Cooper Pediatrics"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "COOPER HEALTH SYSTEM PEDIATRICS DEPARTMENT"] <- "Cooper Pediatrics"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "COOPER HEALTH SYSTEM - PEDIATRICS DEPARTMENT "] <- "Cooper Pediatrics"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "COOPER HEALTH SYSTEM  PEDIATRICS DEPARTMENT"] <- "Cooper Pediatrics"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "COOPER HEALTH SYSTEM - PEDIATRICS DEPARTMENT"] <- "Cooper Pediatrics"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "COOPER PHYSICIANS OFFICES"] <- "Cooper IM"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "COOPER PHYSICIAN OFFICES PA"] <- "Cooper IM"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "Cooper_UHI_Nic"] <- "Cooper IM"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "JEFFREY A KLEEMAN DO"] <- "Fairview"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "LOURDES MEDICAL ASSOCIATES_CAMDEN"] <- "Osborn"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "LOURDES MEDICAL ASSOCIATES_PENNSAUKEN"] <- "Lourdes Pediatrics"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "OSBORN FAMILY PRACTICE_CAMDEN"] <- "Osborn"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "PROJECT HOPE"] <- "Project Hope"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "PROJECT HOPE HOMELESS PROGRAM"] <- "Project Hope"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "RIVER PRIMARY CARE CENTER"] <- "Reliance River"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "ST LUKE'S CATHOLIC MED SVCS"] <- "St. Lukes"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "ST LUKES CATHOLIC MEDICAL SERVICES INC"] <- "St. Lukes"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "ST LUKE'S CATHOLIC MED  SVCS"] <- "St. Lukes"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "ST LUKE’S CATHOLIC MEDICAL SERVICES INC"] <- "St. Lukes"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "ST LUKE'S CATHOLIC MEDICAL SERVICES INC"] <- "St. Lukes"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "VIRTUA FAMILY MEDICINE-COOPER RIVER"] <- "Virtua"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "VIRTUA MEDICAL GROUP"] <- "Virtua"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "VIRTUA MEDICAL GROUP PA"] <- "Virtua"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "NELSON HOMER L"] <- "Broadway Community"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "NELSON, HOMER"] <- "Broadway Community"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "EAST CAMDEN MEDICAL PRACTICE"] <- "East Camden"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "NGUYEN BAO D"] <- "Bao Nguyen"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "BROADWAY COMM HLTH CARE"] <- "Broadway Community"
-  AllPayers$PRACTICE[AllPayers$VEND_FULL_NAME == "COOPER WOODLYNNE"] <- "Cooper Woodlynne"
+  x$PRACTICE[x$VEND_FULL_NAME == "ACOSTA RAMON"] <- "Acosta"
+  x$PRACTICE[x$VEND_FULL_NAME == "RELIANCE BROADWAY_CAMDEN"] <- "Reliance Broadway"
+  x$PRACTICE[x$VEND_FULL_NAME == "NEW JERSEY MEDICAL AND HEALTH ASSOCIATES LLC"] <- "Reliance Broadway"
+  x$PRACTICE[x$VEND_FULL_NAME == "RELIANCE MEDICAL GROUP"] <- "Reliance Broadway"
+  x$PRACTICE[x$VEND_FULL_NAME == "RELIANCE MEDICAL GROUP LLC"] <- "Reliance Broadway"
+  x$PRACTICE[x$VEND_FULL_NAME == "RELIANCE BROADWAY_PENNSAUKEN"] <- "Reliance Pennsauken"
+  x$PRACTICE[x$VEND_FULL_NAME == "CAMCARE HEALTH CORPORATION"] <- "CAMcare"
+  x$PRACTICE[x$VEND_FULL_NAME == "COOPER AMBULATORY PEDIATRICS"] <- "Cooper Pediatrics"
+  x$PRACTICE[x$VEND_FULL_NAME == "COOPER FAMILY MEDICINE"] <- "Cooper Family"
+  x$PRACTICE[x$VEND_FULL_NAME == "COOPER FAMILY MEDICINE PC"] <- "Cooper Family"
+  x$PRACTICE[x$VEND_FULL_NAME == "COOPER PEDIATRICS"] <- "Cooper Pediatrics"
+  x$PRACTICE[x$VEND_FULL_NAME == "COOPER HEALTH SYSTEM PEDIATRICS DEPARTMENT"] <- "Cooper Pediatrics"
+  x$PRACTICE[x$VEND_FULL_NAME == "COOPER HEALTH SYSTEM - PEDIATRICS DEPARTMENT "] <- "Cooper Pediatrics"
+  x$PRACTICE[x$VEND_FULL_NAME == "COOPER HEALTH SYSTEM  PEDIATRICS DEPARTMENT"] <- "Cooper Pediatrics"
+  x$PRACTICE[x$VEND_FULL_NAME == "COOPER HEALTH SYSTEM - PEDIATRICS DEPARTMENT"] <- "Cooper Pediatrics"
+  x$PRACTICE[x$VEND_FULL_NAME == "COOPER PHYSICIANS OFFICES"] <- "Cooper IM"
+  x$PRACTICE[x$VEND_FULL_NAME == "COOPER PHYSICIAN OFFICES PA"] <- "Cooper IM"
+  x$PRACTICE[x$VEND_FULL_NAME == "Cooper_UHI_Nic"] <- "Cooper IM"
+  x$PRACTICE[x$VEND_FULL_NAME == "JEFFREY A KLEEMAN DO"] <- "Fairview"
+  x$PRACTICE[x$VEND_FULL_NAME == "LOURDES MEDICAL ASSOCIATES_CAMDEN"] <- "Osborn"
+  x$PRACTICE[x$VEND_FULL_NAME == "LOURDES MEDICAL ASSOCIATES_PENNSAUKEN"] <- "Lourdes Pediatrics"
+  x$PRACTICE[x$VEND_FULL_NAME == "OSBORN FAMILY PRACTICE_CAMDEN"] <- "Osborn"
+  x$PRACTICE[x$VEND_FULL_NAME == "PROJECT HOPE"] <- "Project Hope"
+  x$PRACTICE[x$VEND_FULL_NAME == "PROJECT HOPE HOMELESS PROGRAM"] <- "Project Hope"
+  x$PRACTICE[x$VEND_FULL_NAME == "RIVER PRIMARY CARE CENTER"] <- "Reliance River"
+  x$PRACTICE[x$VEND_FULL_NAME == "ST LUKE'S CATHOLIC MED SVCS"] <- "St. Lukes"
+  x$PRACTICE[x$VEND_FULL_NAME == "ST LUKES CATHOLIC MEDICAL SERVICES INC"] <- "St. Lukes"
+  x$PRACTICE[x$VEND_FULL_NAME == "ST LUKE'S CATHOLIC MED  SVCS"] <- "St. Lukes"
+  x$PRACTICE[x$VEND_FULL_NAME == "ST LUKE’S CATHOLIC MEDICAL SERVICES INC"] <- "St. Lukes"
+  x$PRACTICE[x$VEND_FULL_NAME == "ST LUKE'S CATHOLIC MEDICAL SERVICES INC"] <- "St. Lukes"
+  x$PRACTICE[x$VEND_FULL_NAME == "VIRTUA FAMILY MEDICINE-COOPER RIVER"] <- "Virtua"
+  x$PRACTICE[x$VEND_FULL_NAME == "VIRTUA MEDICAL GROUP"] <- "Virtua"
+  x$PRACTICE[x$VEND_FULL_NAME == "VIRTUA MEDICAL GROUP PA"] <- "Virtua"
+  x$PRACTICE[x$VEND_FULL_NAME == "NELSON HOMER L"] <- "Broadway Community"
+  x$PRACTICE[x$VEND_FULL_NAME == "NELSON, HOMER"] <- "Broadway Community"
+  x$PRACTICE[x$VEND_FULL_NAME == "EAST CAMDEN MEDICAL PRACTICE"] <- "East Camden"
+  x$PRACTICE[x$VEND_FULL_NAME == "NGUYEN BAO D"] <- "Bao Nguyen"
+  x$PRACTICE[x$VEND_FULL_NAME == "BROADWAY COMM HLTH CARE"] <- "Broadway Community"
+  x$PRACTICE[x$VEND_FULL_NAME == "COOPER WOODLYNNE"] <- "Cooper Woodlynne"
+
+  # Merges x data
+  AllPayers <- rbind(x, y3)
 
   # Sets as dataframe
   AllPayers <- as.data.frame(AllPayers)
-
-  # Removes fields that don't need to go to CareEvolution from the CareEvolution version of the file
-  AllPayers$MEMB_LANGUAGE <- NULL
-  AllPayers$MEMB_ETHNICITY <- NULL
 
   # Adds last capitation
   AllPayers$LastCapitationDate <- format(Sys.time(), "%m/01/%Y")
 
   # Remove "U" from string to match TrackVia Subscriber IDs
   AllPayers$SUBSCRIBER_ID <- gsub("U", "", AllPayers$SUBSCRIBER_ID)
-
-  # Prints warning message if dataframe is missing practices
-
-  # Creates data frame with records a practice
-  # These need to be fixed before sending to Nick (!!!)
 
   print(AllPayers)
 }
